3b8b74214a1dc01dccdc03e7e296953caa46d458
Mayank Mishra <mi5294759@gmail.com>
2026-02-18 02:21:57 +0530

    Delete Spotify Clone Html Css Js directory

--- Spotify Clone Html Css Js/Js/Pages/box2/mainPagefrontPage.js
+++
@@ -1,6 +0,0 @@
-const box2MannashBoxShowAll = document.getElementById("box2_mannashBox_showAll")
-const mainPageL1AllPodcastMusic = document.getElementById("main_page_l1_all_podcast_music")
-
-box2MannashBoxShowAll.addEventListener("click",()=>{
-    mainPageL1AllPodcastMusic.style.display = "none"
-})
--- Spotify Clone Html Css Js/Js/Pages/box3/box3Song.js
+++
@@ -1,9 +0,0 @@
-const box3Main = document.getElementById("box3_MainPage")
-const box3Song1 = document.getElementById("box3_song1")
-
-
-firstSongBtn.addEventListener("click", () => {
-
-    box3Main.style.display = "none"
-    box3Song1.style.display = "block"
-})
